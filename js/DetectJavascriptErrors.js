30977f30e25e63fba540ec8db95d4ad37a6cf2b1
Nuno Oliveira <nmoliveira@gmail.com>
2013-04-27 01:38:41 +0100

    Initial version

---
+++ js/DetectJavascriptErrors.js
@@ -0,0 +1,16 @@
+// By: Nuno Oliveira
+// https://github.com/nmoliveira/DetectJavascriptErrors
+
+var DetectJavascriptErrors = function() {
+  // @stopDefaultHandler: if set to true the default errror handler won't be fired 
+  var start = function(stopDefaultHandler) {
+    window.onerror = function(message, url, lineNumber) {  
+      // handle error as you want
+      alert('Javascript Errors Detected!!');
+      return fireDefaultHandler;
+    }; 
+  };
+  return {
+    start: start
+  }   
+}();
